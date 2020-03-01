37270d4ead756c6452e2208c364e864c3cb0d82d
Tery Lim <kumabotz@gmail.com>
2020-03-01 20:56:40 +1300

    Initial commit

---
+++ Assets/Scripts/Level1HandlePrefabLifecycleTraditional.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1HandlePrefabLifecycleTraditional : MonoBehaviour
+{
+    [SerializeField] private Transform spawnAnchor = null;
+    [SerializeField] private float separation = 1.1f;
+    [SerializeField] private int instanceCount = 10;
+    [SerializeField] private GameObject prefabReference = null;
+
+    List<GameObject> _instances = new List<GameObject>();
+
+    public void HandleLifecycle()
+    {
+        var hasSpawnedInstances = _instances.Count > 0;
+        if (hasSpawnedInstances)
+        {
+            Despawn();
+        }
+        else
+        {
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        for (var i = 0; i < instanceCount; i++)
+        {
+            var newGameObject = Instantiate(prefabReference, spawnAnchor.position + i * separation * Vector3.right, spawnAnchor.rotation);
+            _instances.Add(newGameObject);
+        }
+    }
+
+    private void Despawn()
+    {
+        foreach (var instance in _instances)
+        {
+            Destroy(instance);
+        }
+        _instances.Clear();
+    }
+}
