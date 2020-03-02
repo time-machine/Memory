bd1b9c75e0352a576725390d8d8b44a86914376f
Tery Lim <kumabotz@gmail.com>
2020-03-02 19:32:24 +1300

    Lvl 2

--- Assets/Scripts/Level1HandlePrefabLifecycleTraditional.cs
+++ Assets/Scripts/Level1HandlePrefabLifecycleTraditional.cs
@@ -1,43 +1,61 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
-public class Level1HandlePrefabLifecycleTraditional : MonoBehaviour
+namespace Assets.Scripts
 {
-    [SerializeField] private Transform spawnAnchor = null;
-    [SerializeField] private float separation = 1.1f;
-    [SerializeField] private int instanceCount = 10;
-    [SerializeField] private GameObject prefabReference = null;
-
-    List<GameObject> _instances = new List<GameObject>();
-
-    public void HandleLifecycle()
+    public class Level1HandlePrefabLifecycleTraditional : MonoBehaviour
     {
-        var hasSpawnedInstances = _instances.Count > 0;
-        if (hasSpawnedInstances)
-        {
-            Despawn();
-        }
-        else
-        {
-            Spawn();
-        }
-    }
+        [SerializeField] private Transform spawnAnchor = null;
+        [SerializeField] private float separation = 1.1f;
+        [SerializeField] private int instanceCount = 10;
+        [SerializeField] private AssetReference prefabReference = null;
 
-    private void Spawn()
-    {
-        for (var i = 0; i < instanceCount; i++)
-        {
-            var newGameObject = Instantiate(prefabReference, spawnAnchor.position + i * separation * Vector3.right, spawnAnchor.rotation);
-            _instances.Add(newGameObject);
-        }
-    }
+        private AsyncOperationHandle<GameObject> _asyncOperationHandle;
+        private readonly List<GameObject> _instances = new List<GameObject>();
 
-    private void Despawn()
-    {
-        foreach (var instance in _instances)
+        public void HandleLifecycle()
         {
-            Destroy(instance);
+            var hasSpawnedInstances = _asyncOperationHandle.IsValid();
+            if (hasSpawnedInstances)
+            {
+                Despawn();
+            }
+            else
+            {
+                Spawn();
+            }
         }
-        _instances.Clear();
+
+        private void Spawn()
+        {
+            _asyncOperationHandle = prefabReference.LoadAssetAsync<GameObject>();
+            _asyncOperationHandle.Completed += handle =>
+            {
+                var prefab = handle.Result;
+                SpawnPrefabs(prefab);
+            };
+        }
+
+        private void SpawnPrefabs(GameObject prefab)
+        {
+            for (var i = 0; i < instanceCount; i++)
+            {
+                var newGameObject = Instantiate(prefab, spawnAnchor.position + i * separation * Vector3.right,
+                    spawnAnchor.rotation);
+                _instances.Add(newGameObject);
+            }
+        }
+
+        private void Despawn()
+        {
+            foreach (var instance in _instances)
+            {
+                Destroy(instance);
+            }
+            _instances.Clear();
+            Addressables.Release(_asyncOperationHandle);
+        }
     }
 }
